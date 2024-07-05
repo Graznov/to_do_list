14030d9d2535f9afd4b5fa03d0414d6929f4a9b8
JL-Asus_Graznov <graznov@me.com>
2024-07-05 13:58:56 +0300

    sdsdfasrfgsdgfh

--- src/Components/WorkWind/WorkWind.tsx
+++ src/Components/WorkWind/WorkWind.tsx
@@ -1,10 +1,32 @@
 import styles from './workWind.module.css'
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+function WorkWind() {
 
 
-function WorkWind(props) {
+
     return (
-        <div>
-            <h1>WorkWind</h1>
+        <div className={cx('work_container')}>
+
+            <div className={cx('work_container_leftPanel')}>
+
+                <div className={cx('work_container_leftPanel_user')}>
+                    <img
+                        src="./public/user_avatar.jpeg"
+                        alt="user ava"
+                    />
+                    <h3>User01</h3>
+                </div>
+
+
+            </div>
+
+            <div className={cx('work_container_rightPanel')}>
+
+            </div>
+
         </div>
     );
 }
