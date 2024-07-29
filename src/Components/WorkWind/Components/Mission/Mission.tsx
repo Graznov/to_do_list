a7d749b2cd6482deb46dde3fe31da5a79d6ade9b
JL-Huawei_Graznov <graznov@me.com>
2024-07-29 23:05:27 +0300

    missionлоплроп

--- src/Components/WorkWind/Components/Mission/Mission.tsx
+++ src/Components/WorkWind/Components/Mission/Mission.tsx
@@ -6,13 +6,17 @@ const cx = classNames.bind(styles);
 export function Mission({tag, text, id}) {
     return (
         <div className={cx('mission')}>
-            <input
-                className={cx('missCheck')}
-                onChange={()=>console.log(`CHEKED ${tag}`)}
-                type="checkbox"
+            <label>
+                <input
+                    className={cx('missCheck')}
+                    onChange={() => console.log(`CHEKED ${tag}`)}
+                    type="checkbox"
                 />
+            </label>
+
             <div className={cx('missTag')}>{tag}</div>
             <div className={cx('missText')}>{text}</div>
         </div>
     );
-};
+}
+
