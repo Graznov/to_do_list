b19e273918c9e29ec33b3158bf86394bce3b20cb
JL-Asus_Graznov <graznov@me.com>
2024-11-11 19:11:54 +0300

    sdfsgasdfwerasedf

--- src/Store/authService.ts
+++ src/Store/authService.ts
@@ -28,29 +28,3 @@ export class AuthService {
 }
 
 export const authService = new AuthService();
-//
-// class AuthService {
-//     public async signup(data: RegistrationRequestData): Promise<AxiosResponse<AuthDataResponse>> {
-//         return await apiClient.post('/registration', data);
-//     }
-//
-//     public async signin(data: LoginRequestData): Promise<AxiosResponse<AuthDataResponse>> {
-//         return await apiClient.post('/login', data);
-//     }
-//
-//     public async checkAuth(): Promise<AxiosResponse<AuthDataResponse>> {
-//         return await axios.post(
-//             `${import.meta.env.VITE_API_URL}/refresh`,
-//             {},
-//             {
-//                 withCredentials: true,
-//             },
-//         );
-//     }
-//
-//     public async logout() {
-//         return await apiClient.post('/logout');
-//     }
-// }
-//
-// export const authService = new AuthService();
